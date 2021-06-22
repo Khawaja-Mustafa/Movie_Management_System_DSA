f76ddca6d4f036e6598de30fe83864714b4f73e4
Khawaja Mustafa Shahid <81183116+Khawaja-Mustafa@users.noreply.github.com>
2021-06-22 12:32:11 +0500

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 struct registerddl
 {
-	string ID, Name, Address, PhoneNo, Student, Institute;
+	int ID;
+	string Name, Address, PhoneNo, Student, Institute;
 	registerddl* next;
 	registerddl* prev;
 };
@@ -13,29 +14,28 @@ registerddl* R_head = NULL;
 void RegisterUser()
 {
 	registerddl* newnode = new registerddl();
-	string iname, iId, iAddress, iPhone, student, institute;
-	cout << "Enter Name:";
+	int iId;
+	string iname, iAddress, iPhone, student, institute;
+	cout << "\t\t\tEnter ID:";
+	cin >> iId;
+	cout << "\t\t\tEnter Name:";
 	cin.ignore();
 	getline(cin, iname);
-	cout << "Enter ID:";
-	cin.ignore();
-	getline(cin, iId);
-	cout << "Enter Address:";
-	cin.ignore();
+	cout << "\t\t\tEnter Address:";
 	getline(cin, iAddress);
-	cout << "Enter Phone No:";
-	cin.ignore();
+	cout << "\t\t\tEnter Phone No:";
 	getline(cin, iPhone);
-	cout << "student(Yes Or No):";
+	cout << "\t\t\tstudent(Yes Or No):";
 	cin >> student;
-	if (student == "yes")
+	if (student == "yes"||student == "Yes")
 	{
-		cout << "Enter Institute Name : ";
-		cin >> institute;
+		cout << "\t\t\tEnter Institute Name : ";
+		cin.ignore();
+		getline(cin, institute);
 	}
 	else
 	{
-		return;
+		cout << "\t\t\tEnter Institute Name : None\n";
 	}
 	newnode->ID = iId;
 	newnode->Address = iAddress;
@@ -58,25 +58,79 @@ void RegisterUser()
 			temp = temp->next;
 		}
 		temp->next = newnode;
-		newnode->prev = temp->next;
+		newnode->prev = temp;
 		newnode->next = NULL;
-	}
+	}	
 }
-void R_display()
+void R_display(int idisplay)
 {
+	fstream file, file2;
 	registerddl* temp = R_head;
+	file2.open("Registered_Users.txt", ios::out | ios::in | ios::trunc);
+	file.open("Registered_Users.txt", ios::out| ios::in | ios::app);
+	if(idisplay==0)
+	{
 	while (temp != NULL)
 	{
-		cout << "Name : " << temp->Name << endl;
-		cout << "ID : " << temp->ID << endl;
-		cout << "Address : " << temp->Address << endl;
-		cout << "Phone No : " << temp->PhoneNo << endl;
-		cout << "student(Yes Or No) : " << temp->Student << endl;
-		cout << "Institute Name : " << temp->Institute << endl;
+		file << "Name : " << temp->Name << endl;
+		file << "ID : " << temp->ID << endl;
+		file << "Address : " << temp->Address << endl;
+		file << "Phone No : " << temp->PhoneNo << endl;
+		file << "student(Yes Or No) : " << temp->Student << endl;
+		file << "Institute Name : " << temp->Institute << endl;
 		temp = temp->next;
 	}
+	file2.close();
+	file.close();
+	}
+	else if(idisplay==1)
+	{
+		while (temp != NULL)
+		{
+			cout << "ID : " << temp->ID << endl;
+			cout << "Name : " << temp->Name << endl;
+			cout << "Address : " << temp->Address << endl;
+			cout << "Phone No : " << temp->PhoneNo << endl;
+			cout << "student(Yes Or No) : " << temp->Student << endl;
+			cout << "Institute Name : " << temp->Institute << endl;
+			temp = temp->next;
+		}
+	}
+
 }
-void R_delete(string id)
+void R_update()
+{
+		int id;
+		if (R_head == NULL)
+		{
+			cout << "\n\t NULL";
+		}
+		else
+		{
+			registerddl* temp = R_head;
+			cout << "\n\t\t Enter Id You Want To Update = ";
+			cin >> id;
+			cout << "\n";
+			while (temp != NULL)
+			{
+				if (id == temp->ID)
+				{
+					cout << "\n\t\t The Id : "; 
+					cin >> temp->ID;
+					cout << "\n\t\t The Name : "; 
+					cin >> temp->Name;
+					cout << "\n\t\t The Phone No# : "; 
+					cin >> temp->PhoneNo;
+					cout << "\n\t\tStudent(Yes Or No) : "; 
+					cin >> temp->Student;
+					cout << "\n\t\t Institute Name "; 
+					cin >> temp->Institute;
+				}
+				temp = temp->next;
+			}
+		}
+}
+void R_delete(int id)
 {
 	registerddl* p, * q;
 	q = R_head;
@@ -85,7 +139,6 @@ void R_delete(string id)
 	{
 		R_head = p;
 		delete(q);
-		p->prev = NULL;
 	}
 	else
 	{
@@ -127,7 +180,7 @@ int main()
 		//goto Walk_In_Customer;
 	}
 
-	//Admin Block
+//+++++++++++++++++++++++++++++Admin Block+++++++++++++++++++++++++++++++++++++++++++++++++++++
 	Admin_Login:
 	int Admin_ID;
 	string Admin_Pass;
@@ -172,12 +225,73 @@ Admin_Block:
 	cout << "\t\t\t\t\t~~~~~ ADMIN ~~~~~\n";
 	cout << "\n\n\t\t1) Register Users \n\n\t\t2) Book and Update Tickets \n\n\t\t3) Add or Update Movies \n\n\t\t4) Print or Update Bill \n\n\t\t5) View Movies \n\n\t\tYour Choice: ";
 	int Admin_Choice;
+	int display;
 	cin >> Admin_Choice;
 	if(Admin_Choice == 1)
 	{
+		Reg_User_Page:
 		system("cls");
 		cout << "\t\t\t\t~~~~~ Movie Management System ~~~~~\n";
 		cout << "\t\t\t\t~~~~~ Register New Users ~~~~~\n\n";
+		cout << "\n\n\t\t1) Register New User \n\n\t\t2) Update User \n\n\t\t3) Delete User \n\n\t\t4) Display Reg User \n\n\t\t5) Back Page \n\n\t\tYour Choice: ";
+		int Reg_User_Choice;
+		cin >> Reg_User_Choice;
+		if (Reg_User_Choice == 1)
+		{
+			system("cls");
+			cout << "\t\t\t\t~~~~~ Movie Management System ~~~~~\n";
+			cout << "\t\t\t\t~~~~~ Register New Users ~~~~~\n\n";
+			display = 0;
+			RegisterUser();
+			R_display(display);
+			system("pause");
+			system("cls");
+			goto Reg_User_Page;
+		}
+		else if(Reg_User_Choice==2)
+		{
+			system("cls");
+			cout << "\t\t\t\t~~~~~ Movie Management System ~~~~~\n";
+			cout << "\t\t\t\t~~~~~ Register New Users ~~~~~\n\n";
+			display = 0;
+			R_update();
+			R_display(display);
+			system("pause");
+			system("cls");
+			goto Reg_User_Page;
+		}
+		else if (Reg_User_Choice == 3)
+		{
+			system("cls");
+			cout << "\t\t\t\t~~~~~ Movie Management System ~~~~~\n";
+			cout << "\t\t\t\t~~~~~ Register New Users ~~~~~\n\n";
+			display = 0;
+			int id;
+			cout << "\n\n\t\tEnter the id of the user you want to delete: ";
+			cin >> id;
+			R_delete(id);
+			R_display(display);
+			system("pause");
+			system("cls");
+			goto Reg_User_Page;
+		}
+		else if (Reg_User_Choice == 4)
+		{
+			system("cls");
+			cout << "\t\t\t\t~~~~~ Movie Management System ~~~~~\n";
+			cout << "\t\t\t\t~~~~~ Register New Users ~~~~~\n\n";
+			display = 1;
+			R_display(display);
+			system("pause");
+			system("cls");
+			goto Reg_User_Page;
+		}
+		else if (Reg_User_Choice == 5) 
+		{
+			system("cls");
+			goto Admin_Block;
+		}
+		
 	}
 	else if(Admin_Choice == 2)
 	{
